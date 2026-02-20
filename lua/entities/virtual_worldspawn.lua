4a1773bba5624406bcd4f0f9a4b64c5c1ec09540
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2026-02-19 17:27:43 -0800

    prototype collision

--- lua/entities/virtual_worldspawn.lua
+++ lua/entities/virtual_worldspawn.lua
@@ -7,17 +7,23 @@ ENT.Spawnable = false
 
 ENT.Model = Model( "models/props_junk/wood_crate002a.mdl" )
 
+local HAMMER_MAX_MAP_LENGTH = 32767
+local HAMMER_MAX_MAP_VECTOR = Vector( HAMMER_MAX_MAP_LENGTH, HAMMER_MAX_MAP_LENGTH, HAMMER_MAX_MAP_LENGTH )
+
 function ENT:Initialize()
     self:SetModel( self.Model )
 
-    self:PhysicsInit( SOLID_VPHYSICS )
-    self:SetMoveType( MOVETYPE_VPHYSICS )
-    self:SetSolid( SOLID_VPHYSICS )
+    self:PhysicsInit( SOLID_BBOX )
+    self:SetMoveType( MOVETYPE_NONE )
+    self:SetSolid( SOLID_BBOX )
 
     local phys = self:GetPhysicsObject()
     if IsValid( phys ) then
         phys:EnableMotion( false )
     end
+
+    self:EnableCustomCollisions()
+    self:SetCollisionBounds( HAMMER_MAX_MAP_VECTOR * -1, HAMMER_MAX_MAP_VECTOR )
 
     -- self.VERTICES = {}
     -- local scale = 100
@@ -74,4 +80,111 @@ function ENT:Draw()
     --     render.SetMaterial( MAT_WIREFRAME )
     --     self._mesh:Draw()
     -- end
+end
+
+local function FlagsToString( valueFlags )
+    local flags = {}
+
+    local function TestAndTrack( flagName )
+        local value = _G[flagName]
+        if value == nil then error("Invalid variable") end
+
+        if bit.band( valueFlags, value ) == value then
+            table.insert( flags, flagName )
+        end
+    end
+
+    -- TestAndTrack( "CONTENTS_EMPTY" )
+    TestAndTrack( "CONTENTS_SOLID" )
+    TestAndTrack( "CONTENTS_WINDOW" )
+    TestAndTrack( "CONTENTS_AUX" )
+    TestAndTrack( "CONTENTS_GRATE" )
+    TestAndTrack( "CONTENTS_SLIME" )
+    TestAndTrack( "CONTENTS_WATER" )
+    TestAndTrack( "CONTENTS_BLOCKLOS" )
+    TestAndTrack( "CONTENTS_OPAQUE" )
+    TestAndTrack( "CONTENTS_TESTFOGVOLUME" )
+    TestAndTrack( "CONTENTS_TEAM4" )
+    TestAndTrack( "CONTENTS_TEAM3" )
+    TestAndTrack( "CONTENTS_TEAM1" )
+    TestAndTrack( "CONTENTS_TEAM2" )
+    TestAndTrack( "CONTENTS_IGNORE_NODRAW_OPAQUE" )
+    TestAndTrack( "CONTENTS_MOVEABLE" )
+    TestAndTrack( "CONTENTS_AREAPORTAL" )
+    TestAndTrack( "CONTENTS_PLAYERCLIP" )
+    TestAndTrack( "CONTENTS_MONSTERCLIP" )
+    TestAndTrack( "CONTENTS_CURRENT_0" )
+    TestAndTrack( "CONTENTS_CURRENT_180" )
+    TestAndTrack( "CONTENTS_CURRENT_270" )
+    TestAndTrack( "CONTENTS_CURRENT_90" )
+    TestAndTrack( "CONTENTS_CURRENT_DOWN" )
+    TestAndTrack( "CONTENTS_CURRENT_UP" )
+    TestAndTrack( "CONTENTS_DEBRIS" )
+    TestAndTrack( "CONTENTS_DETAIL" )
+    TestAndTrack( "CONTENTS_HITBOX" )
+    TestAndTrack( "CONTENTS_LADDER" )
+    TestAndTrack( "CONTENTS_MONSTER" )
+    TestAndTrack( "CONTENTS_ORIGIN" )
+    TestAndTrack( "CONTENTS_TRANSLUCENT" )
+
+    return table.concat( flags, ", " )
+end
+
+function ENT:TestCollision( startpos, delta, isbox, extents, mask )
+
+    -- print(FlagsToString(33636363))
+
+    -- if CLIENT then return end
+
+    -- print("Virtual worldspawn trace hit!")
+    -- print("Start", startpos)
+    -- print("Delta", delta)
+    -- print("End", startpos + delta)
+    -- print("Is box", isbox)
+    -- print("Extents", extents)
+    -- print("Mask", FlagsToString(mask))
+    -- print("Tick", engine.TickCount())
+    -- print()
+
+    -- if mask == 69 then
+    --     print("Trip!")
+    --     return
+    -- end
+
+    -- if mask == MASK_PLAYERSOLID and delta.z ~= -3 then
+    --     return
+    -- end
+
+    -- if delta.z ~= -3 then return end
+
+
+    -- Trap for debugging
+
+    -- a = true
+    -- while a do
+    --     a = true
+    -- end
+
+    -- print("abc")
+
+    local endPos = startpos + delta
+    if endPos.z <= -118 and delta.z == -3 then
+        local newHitPos = Vector( endPos.x, endPos.y, -100 )
+
+        -- Entity(1):SetGroundEntity(self)
+
+        -- print("Returned")
+        return {
+            HitPos = newHitPos,
+            Fraction = 0.9, -- anything but 1
+            Normal = Vector( 0, 0, 0.8 ),
+
+            Hit = true,
+            HitWorld = true,
+            Entity = self,
+            Contents = CONTENTS_SOLID,
+            SurfaceFlags = SURF_SKY,
+        }
+    end
+
 end
