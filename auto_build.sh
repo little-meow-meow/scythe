555b6958176434d877d233bc14c347430571d2a2
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2026-05-10 03:44:24 -0700

    tool: reduce autorefresh wait overhead

--- auto_build.sh
+++ auto_build.sh
@@ -5,6 +5,8 @@
 inotifywait -r -m -e modify src |
     while read file_path file_event file_name; do
         cyan build --prune
-        touch lua/autorun/scythe_loader_sh.lua
+        if [[ $file_path =~ "scythe_loader_sh.lua$" ]]; then
+            touch lua/autorun/scythe_loader_sh.lua
+        fi
     done
 
